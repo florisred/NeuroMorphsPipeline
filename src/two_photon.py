6fe7d9693bea92036f1d07d790870d37dc1d3a23
Floris <florishoogenboom@proton.me>
2026-03-05 14:47:55 +0100

    big rewrite
    removed a lot of redundant variables, functions, etc. Made it possible to work with multiple sessions, made the labels dataframe a lot more workable and readable, and made all of it work with it

--- src/two_photon.py
+++ src/two_photon.py
@@ -1,3 +1,4 @@
+from itertools import groupby
 from os.path import join
 from pathlib import Path
 import pandas as pd
@@ -14,58 +15,40 @@ class Twophoton:
         self.data_df = None
         self.labels = None
         self.processed_df = None
-        self.pca_coords=None
-        self.variance_explained=None
-        self.metadata_list = [
-                "meta/mouse",
-                "meta/date",
-                "meta/sess_repN",
-                "meta/sess_index"
-            ]
         self.labels_list = ["y/pair_key", "y/step_index", "y/src_cat", "y/dst_cat", "y/norm_step", "y/stim_type"]
-        # self.partial_pca_full_morphs = None
-        # self.partial_pca_full_morphs_labels = None
 
-
-
-
-
-    def load_2p_data(self, data_dir, data_location, label_location):
+    def load_2p_data(self, session_dirs, data_location, label_location):
         """
-        Loads 2-photon imaging data from the specified directory and file locations. The function retrieves the data
-        and labels, applies necessary preprocessing, and stores them into appropriate attributes for further use.
 
-        :param data_dir: Directory containing the 2-photon imaging data files.
-        :type data_dir: str
-        :param data_location: Key or path specifying the location of the dataset in the .h5 file.
-        :type data_location: str
-        :param label_location: Key or path specifying the location of the labels in the .h5 file.
-        :type label_location: str
-        :return: None
         """
-        two_photon_folder = join(data_dir, "2p_data")
-        file = list(Path(two_photon_folder).glob("*.h5"))
-        if len(file) != 1: raise AssertionError("Need exactly one .hy file")
-        file = file[0]
-        f = h5py.File(file, 'r')
-        self.data_df = pd.DataFrame(f[data_location]).dropna(axis=0, how='all')
-        self.labels = self._load_list(f, self.labels_list)
+        data_dfs = []
+        for session_dir in session_dirs:
+
+            two_photon_folder = join(session_dir, "2p_data")
+            file = list(Path(two_photon_folder).glob("*.h5"))
+            if len(file) != 1: raise AssertionError(f"Need exactly one .hy file in {two_photon_folder}")
+            f = h5py.File(file[0], 'r')
+            data = pd.DataFrame(f[data_location]).dropna(axis=0, how='all')
+            labels = self._load_list(f, self.labels_list)
+            grouped_data, index_df = self._calc_mean_per_stimulus(data, labels)
+            grouped_data.index = index_df['morph_name']
+            data_dfs.append(grouped_data)
+
+        # concatinate and scale
+        combined_df = pd.concat(data_dfs, axis=1, join='inner')
+        index_df= combined_df.index.to_frame(index=False, name='morph_name')
+        labels_df = index_df.merge(labels.drop_duplicates('morph_name'), on='morph_name', how='left')
+
+        scaler = StandardScaler()
+        scaled_combined_df = pd.DataFrame(scaler.fit_transform(combined_df))
+        self.data_df = scaled_combined_df
+        self.labels = labels_df
 
 
 
-    def partial_pca_full_morphs(self, choose_transitions = False, ):
+
+    def perform_pca_subset(self, choose_transitions = False):
         """
-        Executes a partial Principal Component Analysis (PCA) on dataset transitions, either using specific
-        transitions or clustered quadruplet chains. The method processes data and labels related to morphologies,
-        combines them as needed, and performs the analysis to generate results grouped by unique morphologies.
-
-        :param choose_transitions: Indicates whether to use specific transitions (if True) or acquire them
-            through clustered quadruplet chains (if False).
-        :type choose_transitions: bool
-
-        :return: A dictionary where keys represent unique morphologies as a concatenated string of identifiers,
-            and values are the results of partial PCA analysis for corresponding sets of transitions.
-        :rtype: dict
         """
         if choose_transitions:
             triplets = [self._io_partial_pca()]
@@ -74,31 +57,22 @@ class Twophoton:
             triplets = self._find_triplets()
             quadruplets = self._find_quadruplet_chains()
         pca_dict = {}
+
         for triplet in triplets:
-            data_chosen_full_morphs, labels_chosen_full_morphs = self._filter_transitions_full_chosen_morphs(chosen_transitions=triplet)
-            data_chosen_morphs, labels_chosen_morphs = self._filter_transitions_all_chosen_morphs(chosen_transitions=triplet)
-            data_chosen_morphs, labels_chosen_morphs = self._concat_all_morphs(data_chosen_full_morphs, labels_chosen_full_morphs, data_chosen_morphs, labels_chosen_morphs)
-            all_morphs = []
-            for trans in triplet:
-                all_morphs.append(trans.split("__"))
-            all_morphs_unique = np.unique(all_morphs)
-            pca_name = ""
-            for name in all_morphs_unique:
-                pca_name += name
-            pca_dict[pca_name] = self._perform_partial_pca(data_chosen_full_morphs, labels_chosen_full_morphs, data_chosen_morphs, labels_chosen_morphs)
+            bases =  np.unique([trip.split("__") for trip in triplet])
+            pca_name = f"triplet"
+            for base in bases: pca_name += f'-{base}'
+            pca_dict[pca_name] = self.perform_pca(
+                transitions=triplet
+            )
 
         for quadruplet in quadruplets:
-            data_chosen_full_morphs, labels_chosen_full_morphs = self._filter_transitions_full_chosen_morphs(chosen_transitions=quadruplet)
-            data_chosen_morphs, labels_chosen_morphs = self._filter_transitions_all_chosen_morphs(chosen_transitions=quadruplet)
-            data_chosen_morphs, labels_chosen_morphs = self._concat_all_morphs(data_chosen_full_morphs, labels_chosen_full_morphs, data_chosen_morphs, labels_chosen_morphs)
-            all_morphs = []
-            for trans in quadruplet:
-                all_morphs.append(trans.split("__"))
-            all_morphs_unique = np.unique(all_morphs)
-            pca_name = ""
-            for name in all_morphs_unique:
-                pca_name = pca_name + '-' + name
-            pca_dict[pca_name] = self._perform_partial_pca(data_chosen_full_morphs, labels_chosen_full_morphs, data_chosen_morphs, labels_chosen_morphs)
+            bases = np.unique([quad.split("__") for quad in quadruplet])
+            pca_name = f"quadruplet"
+            for base in bases: pca_name += f'-{base}'
+            pca_dict[pca_name] = self.perform_pca(
+                transitions=quadruplet
+            )
 
         return pca_dict
 
@@ -131,8 +105,6 @@ class Twophoton:
                 possible_triplets.append((edge1, edge2, edge3))
         print(possible_triplets)
         return possible_triplets
-
-    import itertools
 
     def _find_quadruplet_chains(self):
         """
@@ -187,117 +159,6 @@ class Twophoton:
 
         return found_chains_as_labels
 
-    def _concat_all_morphs(self, data_chosen_full_morphs, labels_chosen_full_morphs, data_chosen_morphs, labels_chosen_morphs):
-        """
-        Concatenates multiple datasets of morphological data and their corresponding labels into
-        a unified dataset and label set. This function ensures that the resulting datasets retain
-        consistent indexing.
-
-        :param data_chosen_full_morphs: A pandas DataFrame containing a dataset of full morphological data.
-        :param labels_chosen_full_morphs: A pandas DataFrame containing the corresponding labels for
-            the data in `data_chosen_full_morphs`.
-        :param data_chosen_morphs: A pandas DataFrame containing another dataset of selected
-            morphological data to be concatenated.
-        :param labels_chosen_morphs: A pandas DataFrame containing the corresponding labels for
-            the data in `data_chosen_morphs`.
-        :return: A tuple with two pandas DataFrames:
-            - The first element contains the combined dataset (`concated_data`).
-            - The second element contains the combined labels (`concated_labels`).
-        """
-
-        concated_data = pd.concat([data_chosen_full_morphs, data_chosen_morphs]).reset_index(drop=True)
-        concated_labels = pd.concat([labels_chosen_full_morphs, labels_chosen_morphs]).reset_index(drop=True)
-        return concated_data, concated_labels
-
-    def _perform_partial_pca(self, data_chosen_full_morphs, labels_chosen_full_morphs, data_chosen_morphs, labels_chosen_morphs):
-        """
-        Performs a partial PCA (Principal Component Analysis) transformation on the given dataset.
-
-        The method applies PCA to the subsets of provided data and computes the PCA coordinates
-        and the explained variance. The labels corresponding to the chosen morphs are
-        returned as-is.
-
-        :param data_chosen_full_morphs: Data representing the full set of morph features to be
-            considered in the PCA transformation.
-        :param labels_chosen_full_morphs: Labels corresponding to the full morph dataset, used
-            for reference or further processing after the PCA transformation.
-        :param data_chosen_morphs: Subset of the morph feature data to use for computing
-            partial PCA. Represents a selected portion of the data.
-        :param labels_chosen_morphs: Labels corresponding to the subset of morph data. These
-            labels are returned alongside the PCA results for context.
-
-        :return: A tuple containing:
-            - `pca_coords`: The computed PCA coordinates for the given data subsets.
-            - `variance_explained`: The proportion of variance explained by each principal
-              component.
-            - `labels_chosen_morphs`: The same labels provided for the chosen morphs,
-              returned for reference.
-        """
-        pca_coords, variance_explained, _ = self.peform_pca(data_chosen_full_morphs, data_chosen_morphs)
-        return pca_coords, variance_explained, labels_chosen_morphs
-
-
-    def _filter_transitions_all_chosen_morphs(self, chosen_transitions):
-        """
-        Filters, scales, and groups activation data based on transitions and suffixes.
-
-        This method processes transitions combined with suffixes to create specific
-        targets. Matching data is then filtered, grouped by certain labels, and
-        averaged. The resulting activation data is standardized using a scaler.
-        It returns the scaled mean activations and the corresponding grouped labels.
-
-        :param chosen_transitions: A list of transition strings to be combined with
-            suffixes for filtering targets.
-        :type chosen_transitions: list of str
-        :return: A tuple containing the scaled mean activations as a DataFrame and
-            the grouped labels as a Series.
-        :rtype: tuple (pd.DataFrame, pd.Series)
-        """
-
-        mask = self.labels["pair_key"].isin(chosen_transitions)
-        data_masked = self.data_df.T[mask]
-        labels_masked = self.labels[mask]
-
-        groups = labels_masked['full_name'].squeeze()
-        mean_activations = data_masked.groupby(groups).mean()
-        scaler = StandardScaler()
-        mean_activations_scaled = scaler.fit_transform(mean_activations)
-        labels_grouped  = mean_activations.index.to_series().reset_index(drop=True)
-        mean_activations_scaled = pd.DataFrame(mean_activations_scaled)
-
-        #prepare the labels df for return
-        labels_unique = self.labels.drop_duplicates(subset=['full_name'])
-        labels_return = pd.DataFrame(labels_grouped).merge(labels_unique, on='full_name', how='left')
-
-
-        return mean_activations_scaled, labels_return
-
-    def _filter_transitions_full_chosen_morphs(self, chosen_transitions):
-
-        ## get the three starting points
-        min_morph = 0
-        max_morph = np.max(self.labels["step_index"].astype(int))
-        suffixes = [f"{min_morph:02d}", f"{max_morph:02d}"]
-        targets = [trans + surf for trans in chosen_transitions for surf in suffixes]
-
-
-        mask = self.labels["full_name"].isin(targets)
-        data_masked = self.data_df.T[mask]
-        labels_masked = self.labels[mask]["full_name"].apply(
-            lambda x: x.split('__')[0] if x.endswith('00') else x.split('__')[1][:-2])
-        groups = labels_masked.squeeze()
-        mean_activations = data_masked.groupby(groups).mean()
-        scaler = StandardScaler()
-        mean_activations_scaled = scaler.fit_transform(mean_activations)
-        mean_activations_scaled = pd.DataFrame(mean_activations_scaled)
-        labels_grouped  = mean_activations.index.to_series().reset_index(drop=True)
-        #prepare the labels df for return
-        labels_unique = self.labels.drop_duplicates(subset=['full_name'])
-        labels_return = pd.DataFrame(labels_grouped).merge(labels_unique, on='full_name', how='left')
-        labels_return['stim_type'] = 'anchor'
-
-        return mean_activations_scaled,labels_return
-
     def _io_partial_pca(self):
         unique_trans = np.unique(self.labels["pair_key"])
         chosen_transitions = []
@@ -318,36 +179,69 @@ class Twophoton:
                 raise AssertionError("what the hell")
         return chosen_transitions
 
+    def _calc_mean_per_stimulus(self, data_df=None, labels=None):
+        if labels is None:
+            labels = self.labels
+        if data_df is None:
+            data_df = self.data_df
+
+        grouped_data = data_df.T.groupby(labels["morph_name"].values).mean()
+        grouped_data_labels = labels.drop_duplicates(subset='morph_name').sort_values(by=['morph_name'])
+        return grouped_data, grouped_data_labels
 
 
+    def perform_pca(self, transitions = None):
+        """"
+        Main function of this class. It takes the grouped activation data and the corresponding labels. It then calculates
+        the so-called anchor points of the stimuli, and adds these. These are the average activation of the full morphs.
 
-    def _calc_mean_per_stimulus_and_scale(self):
-        processed_df = self.data_df.T.groupby(self.labels["full_name"].values).mean()
-        scaler = StandardScaler()
-        processed_df = scaler.fit_transform(processed_df)
-        self.processed_df = processed_df
+        It then creates a PCA space from only these anchor points.
+        It then calculates the coordinates of all activation data based on this PCA space, and returns it.
+        """
+
+        labels = self.labels
+        data_df = self.data_df
+
+        if transitions is not None:
+            data_df, labels = self._filter_transitions(
+                data_df=data_df,
+                labels = labels,
+                transitions=transitions
+            )
+
+        anchors = self._extract_anchors(
+            data_df = data_df,
+            labels = labels
+        )
+        pca = PCA(n_components=(min(len(anchors), 4)))
+        te_stankie = pca.fit_transform(anchors)
+        variance_explained = pca.explained_variance_
+        transformed_data = pca.transform(data_df)
+
+        return transformed_data, variance_explained, labels
+
+    @staticmethod
+    def _filter_transitions(data_df, labels, transitions):
+        mask1 = labels["pair_key"].isin(transitions)  # get all the data from the chosen transitions
+        # find all the anchors
+        relevant_anchors = np.unique(
+            [texture for transition in transitions for texture in transition.split('__')])
+        mask_relevant_morphs = labels['morph_name'].isin(relevant_anchors)
+        # add both masks and filter data
+        final_mask = mask1 | mask_relevant_morphs
+        filtered_data = data_df[final_mask]
+        filtered_labels = labels[final_mask]
+        labels = filtered_labels
+        return filtered_data, labels
 
 
-
-    def peform_pca(self, data_full_morphs=None, data_all=None):
-        self._calc_mean_per_stimulus_and_scale()
-
-
-        if data_full_morphs is None and data_all is None:
-            data = self.processed_df
-            pca = PCA(n_components=(min(len(data), 8)))
-            pca_coords = pca.fit_transform(data)
-            variance_explained = pca.explained_variance_ratio_
-            labels_unique = self.labels.drop_duplicates(subset=['full_name'])
-            return pca_coords, variance_explained, labels_unique
-        else:
-            pca = PCA(n_components=(min(len(data_full_morphs), 8)))
-            pca_coords = pca.fit_transform(data_full_morphs)
-            variance_explained = pca.explained_variance_ratio_
-            pca_coords_all = pca.transform(data_all)
-            labels_unique = self.labels.drop_duplicates(subset=['full_name'])
-
-            return pca_coords_all, variance_explained, labels_unique
+    @staticmethod
+    def _extract_anchors(data_df, labels):
+        """"
+        """
+        is_anchor = labels['stim_type'] == 'anchor'
+        anchor_data = data_df[is_anchor]
+        return anchor_data
 
 
 
@@ -360,12 +254,23 @@ class Twophoton:
             metadata_dataframe[meta_name] = metadata_array.astype(str)
         metadata_dataframe = metadata_dataframe.T
         metadata_df = metadata_dataframe.T
-        col1 = metadata_df.iloc[:, 0].astype(str)
-        col2 = metadata_df.iloc[:, 1].astype(str).str.zfill(2)
-        metadata_df['full_name'] = col1 + col2
+        is_src = metadata_df['stim_type'] == 'endpoint_src'
+        is_dst = metadata_df['stim_type'] == 'endpoint_dst'
+        partial_names = (
+                metadata_df['src_cat'] + "_" +
+                (1-metadata_df['norm_step'].astype(float)).round(2).astype(str) + "_" +
+                metadata_df['dst_cat'] + "_" +
+                metadata_df['norm_step'].astype(float).round(2).astype(str)
+        )
+        metadata_df['morph_name'] = np.where(
+            is_src,
+            metadata_df['src_cat'],
+            np.where(is_dst, metadata_df['dst_cat'], partial_names)
+        )
+        metadata_df['stim_type'] = np.where(
+            is_src | is_dst,
+            'anchor',
+            'morph'
+        )
         return metadata_df
 
-
-    ## krijg alle data in aparte columns
-    # codeer het zodat elke functie daaruit haalt
-    # verwijder "full_name"
