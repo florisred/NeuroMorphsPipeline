40e9bfac9ea9da43c6977533b4db55a452939ea5
Floris <florishoogenboom@proton.me>
2026-03-11 13:00:26 +0100

    data source class
    has an abstract load_data method
    finds stimulus cycles
    has a metadata object with all the metadata

---
+++ data_loader/DataSource.py
@@ -0,0 +1,69 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+import itertools
+from data_loader.TrialMetadata import TrialMetadata
+
+
+class DataSource(ABC):
+    def __init__(
+            self,
+            file_paths: list[Path],
+    ):
+        self.file_paths = file_paths
+        self.data = None
+        self.metadata = TrialMetadata()
+
+
+    @abstractmethod
+    def load_data(self):
+        """
+        Loads data from file
+        """
+        pass
+
+    def find_stimulus_cycles(self, n=3):
+        """
+        Finds all unique n-length cycles (triplets, quadruplets, etc.) in the stimulus transitions.
+
+        :param labels: The metadata DataFrame/dict containing transitions.
+        :param n: The number of nodes in the cycle (3 for triangles, 4 for rings).
+        :return: A list of lists, where each inner list contains the edge keys forming the cycle.
+        """
+        # 1. Standardize edges into a set of frozensets for O(1) lookup
+        # This replaces the slow 'if edge in transitions' check
+        edges_set = set()
+
+        for t in self.metadata.get_pair_keys():
+            edges_set.add(frozenset(t.split("__")))
+
+        unique_nodes = list(set().union(*edges_set))
+        found_cycles = []
+
+        for nodes in itertools.combinations(unique_nodes, n):
+            for p in itertools.permutations(nodes[1:]):
+                path = (nodes[0],) + p
+                valid_cycle = True
+                current_hops = []
+                for i in range(n):
+                    u, v = path[i], path[(i + 1) % n]
+                    edge = frozenset((u, v))
+
+                    if edge in edges_set:
+                        current_hops.append("__".join(sorted([u, v])))
+                    else:
+                        valid_cycle = False
+                        break
+                if valid_cycle:
+                    cycle_id = tuple(sorted(current_hops))
+                    if cycle_id not in [tuple(sorted(c)) for c in found_cycles]:
+                        found_cycles.append(current_hops)
+
+        return found_cycles
+
+    # @abstractmethod
+    # def get_pca(self):
+    #     pass
+    # @abstractmethod
+    # def group_data(self):
+    #     pass
+
