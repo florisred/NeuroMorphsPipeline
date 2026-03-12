9ff820be08694b864dde399a8a5b82520a7bb2ad
Floris <florishoogenboom@proton.me>
2026-03-12 12:57:31 +0100

    create names from list function in utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,6 @@
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
+import numpy as np
 
 
 def scale_session(X):
@@ -11,3 +12,13 @@ def calc_mean_per_stimulus(data_df, labels):
     grouped_data_labels = labels.drop_duplicates(subset='morph_name').sort_values(by=['morph_name'])
     grouped_data.index = grouped_data_labels['morph_name']
     return grouped_data, grouped_data_labels
+
+def create_name_from_list(transition_list: list, first_part: str ='triplet'):
+    all_anchs = []
+    for trans in transition_list:
+        all_anchs.append(trans.split('__'))
+    unique_anchs = np.unique(all_anchs)
+    triplet_name = first_part
+    for anchor in unique_anchs:
+        triplet_name += f'-{anchor}'
+    return triplet_name
