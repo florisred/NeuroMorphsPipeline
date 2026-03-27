743b14026002d6f7a0e612a665bd7f0c20761d6c
Floris <florishoogenboom@proton.me>
2026-03-27 16:52:03 +0100

    Added comments explaining the code to  analyzer.py

--- src/analysis/analyzer.py
+++ src/analysis/analyzer.py
@@ -10,20 +10,26 @@ logger = logging.getLogger(__name__)
 
 
 class Analyzer:
+    """
+    Class that handles the coordination of PCA analyses and Plot creation
+    """
+    # Which plot type is which function
     PLOT_REGISTRY = {
         'interactive': interactive.create_interactive_plot,
         'distances': distances.calculate_distances,
         'subsets': subsets_plot.create_subset_plots,
         'rdm': rdm_plot.rdm_analysis,
-        'rdm_full': rdm_plot.rdm_analysis_full
+        'rdm_full': rdm_plot.rdm_analysis_full,
+        'rdm_split_full': rdm_plot.rdm_analysis_full
     }
-
+    # what type of PCA each plot type requires
     PCA_REQUIREMENTS = {
-        'interactive': 'full',
-        'distances': 'full',
-        'subsets': 'subsets',
-        'rdm': 'subsets',
-        'rdm_full': 'full'
+        'interactive': ['full'],
+        'distances': ['full'],
+        'subsets': ['subsets'],
+        'rdm': ['subsets'],
+        'rdm_full': ['full'],
+        'rdm_split_full': ['full', 'split_full'],
     }
 
     def __init__(self, n_components: int = 3):
@@ -48,9 +54,10 @@ class Analyzer:
             self.pca_manager.clear_cache()
         output_dir.mkdir(parents=True, exist_ok=True)
 
-        required_pca_types = {self.PCA_REQUIREMENTS[p] for p in plot_types if p in self.PCA_REQUIREMENTS}
+        required_pca_types = {tuple(self.PCA_REQUIREMENTS[p]) for p in plot_types if p in self.PCA_REQUIREMENTS}
         for req_type in required_pca_types:
-            self.pca_manager.prepare_data(pca_type=req_type, n_components=n_components, subsets=subsets)
+            # req_type will now be a tuple like ('full',)
+            self.pca_manager.prepare_data(pca_types=req_type, n_components=n_components, subsets=subsets)
 
         for plot_type in plot_types:
             plot_func = self.PLOT_REGISTRY.get(plot_type)
